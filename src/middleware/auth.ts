72695133025791ae46f363470359feac71ff71a2
jaayeshe <jubairaayeshe4@gmail.com>
2024-03-10 20:05:00 -0230

    typo fixed

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -14,7 +14,7 @@ declare global {
 
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
-  issuerBaseURL: process.env.AUTH0_ISSUER__BASE_URL,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
   tokenSigningAlg: "RS256",
 });
 export const jwtParse = async (
