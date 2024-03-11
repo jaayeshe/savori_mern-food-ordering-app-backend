d377cfca1066ad24ed1d742d7ab9a4c36096c4d5
jaayeshe <jubairaayeshe4@gmail.com>
2024-03-11 14:51:28 -0230

    Mongoose schemas for restaurant and menu items

---
+++ src/models/restaurants.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const menuItemSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+//define schema & properties that we want
+//to store for a given restaurant.
+//first we will create a reference to the
+//user document for the user who is creating this restaurant.
+
+const restaurantSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  restaurantName: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  deliveryPrice: { type: Number, required: true },
+  estimatedDeliveryTime: { type: Number, required: true },
+  cuisines: { type: String, required: true },
+  menuItems: [menuItemSchema],
+  imageUrl: { type: String, required: true },
+  lastUpdated: { type: Date, required: true },
+});
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+export default Restaurant;
