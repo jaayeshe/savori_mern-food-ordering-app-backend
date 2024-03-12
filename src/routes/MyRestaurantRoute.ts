999316d03cb1e53899b08950d251de707e4bb5a2
jaayeshe <jubairaayeshe4@gmail.com>
2024-03-11 23:14:54 -0230

    Typo fixed:1025 instead of 1024

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -8,7 +8,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1025, //5MB
+    fileSize: 5 * 1024 * 1024, //5MB
   },
 });
 
